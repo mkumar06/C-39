d76062a20cff899542cf69849a0dc74fcb12b74a
Monish <s.monish2006@gmail.com>
2021-01-18 19:56:04 -0500

    C34

--- sketch.js
+++ sketch.js
@@ -1,29 +1,51 @@
 var ball;
+var db, position
 
 function setup(){
     createCanvas(500,500);
+
+    db = firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    var ballPositionRef = db.ref("ball/position");
+    ballPositionRef.on("value", readPos, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    var ballPositionRef1 = db.ref("ball/position")
+    
+    ballPositionRef1.set({
+        x:ball.x + x, y:ball.y + y
+    })
+}
+
+function readPos(data) {
+    console.log(data.val());
+    position = data.val();
+
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError() {
+    console.log("error");
 }
